f5d8c516943eaa967ab9279ffdd1c1c38c266600
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2024-01-28 18:47:13 +0900

    impl: test libraryを呼び出すmaui appを追加

---
+++ TestMauiApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace TestMauiApp;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+        CounterBtn.Text = TestLibrary.TestLibrary.TestFunc();
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
